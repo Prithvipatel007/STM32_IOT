ade634e3e156d43625262a264b4c28957d19b8c4
Prithvi.Patel <prithvi.patel@soluware.de>
2022-08-27 19:53:20 +0200

    initial commit

---
+++ prov_dev_client/Debug/Core/Src/main.d
@@ -0,0 +1,130 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ ../Core/Inc/stm32l4xx_hal_conf.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4s5xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/deps/umock-c/inc/umock_c/umock_c_prod.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/deps/azure-macro-utils-c/inc/azure_macro_utils/macro_utils.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/deps/azure-macro-utils-c/inc/azure_macro_utils/macro_utils_generated.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_message.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/map.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/strings.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/strings_types.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/crt_abstractions.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_client_version.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/threadapi.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/tickcounter.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/shared_util_options.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/const_defines.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/http_proxy_io.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/xio.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/optionhandler.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_device_client.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_transport_ll.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_client_core_ll.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_client_core_common.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_message.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_client_core.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_device_client_ll.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_device_client_ll.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_client_options.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_device_ll_client.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_transport.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/buffer_.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_client_const.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_device_client.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_device_ll_client.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_security_factory.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/deps/parson/parson.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_service_client/inc/iothub_deviceconfiguration.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/singlylinkedlist.h \
+ D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_service_client/inc/iothub_service_client_auth.h
+../Core/Inc/main.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+../Core/Inc/stm32l4xx_hal_conf.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4s5xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/deps/umock-c/inc/umock_c/umock_c_prod.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/deps/azure-macro-utils-c/inc/azure_macro_utils/macro_utils.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/deps/azure-macro-utils-c/inc/azure_macro_utils/macro_utils_generated.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_message.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/map.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/strings.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/strings_types.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/crt_abstractions.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_client_version.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/threadapi.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/tickcounter.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/shared_util_options.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/const_defines.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/http_proxy_io.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/xio.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/optionhandler.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_device_client.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_transport_ll.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_client_core_ll.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_client_core_common.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_message.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_client_core.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_device_client_ll.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_device_client_ll.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_client/inc/iothub_client_options.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_device_ll_client.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_transport.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/buffer_.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_client_const.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_device_client.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_device_ll_client.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/provisioning_client/inc/azure_prov_client/prov_security_factory.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/deps/parson/parson.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_service_client/inc/iothub_deviceconfiguration.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/c-utility/inc/azure_c_shared_utility/singlylinkedlist.h:
+D:/STM32_IOT/prov_dev_client/Middlewares/Third_Parties/Azure-IoT-SDK-C/iothub_service_client/inc/iothub_service_client_auth.h:
